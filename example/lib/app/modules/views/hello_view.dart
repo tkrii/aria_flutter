7584c726493b2abb44e8745d103d6785da8bd0b4
Tokari <42688334+tkrii@users.noreply.github.com>
2024-07-01 15:11:39 -0600

    2407011511

--- example/lib/app/modules/views/hello_view.dart
+++ example/lib/app/modules/views/hello_view.dart
@@ -1,11 +1,16 @@
 import 'package:example/core/i18n/i18n.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
-import 'package:get/get_utils/src/extensions/internacionalization.dart';
-import 'package:get/get_utils/src/extensions/widget_extensions.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 
+class HelloView extends Scaffold {
+  HelloView({
+    super.key,
+  }) : super(
+          appBar: AppBar(
+            title: Text(I18n.helloTitle),
+          ),
+        );
+}
+/*
 class HelloView extends StatelessWidget {
   const HelloView({super.key});
 
@@ -40,3 +45,6 @@ class HelloView extends StatelessWidget {
     );
   }
 }
+
+
+ */
