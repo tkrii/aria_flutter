eef6cccdd19b11de1e84b808211990dc218dcd79
Tokari <42688334+tkrii@users.noreply.github.com>
2024-08-09 15:40:39 -0600

    arna adapted

---
+++ example/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/media/tokari/Proyectos/aria_flutter/example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/opt/flutter"
+  "PROJECT_DIR=/media/tokari/Proyectos/aria_flutter/example"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/media/tokari/Proyectos/aria_flutter/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/media/tokari/Proyectos/aria_flutter/example/lib/main.dart"
+)
