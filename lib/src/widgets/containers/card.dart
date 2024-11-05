d5894aa7a4c1f8d486372a6093c3d648cd251879
Tokari <42688334+tkrii@users.noreply.github.com>
2024-11-05 13:46:23 -0600

    car upudate 241101346

--- lib/src/widgets/containers/card.dart
+++ lib/src/widgets/containers/card.dart
@@ -1,12 +1,6 @@
 part of '../../theme/scheme_theme.dart';
 
 CardTheme cardTheme(ColorScheme colorScheme) => CardTheme(
-      shape: RoundedRectangleBorder(
-        borderRadius: Utils.mediumBorderRadius,
-        side: Utils.borderSide(
-          colorScheme.outlineVariant,
-        ),
-      ),
       elevation: 2.0,
       surfaceTintColor: colorScheme.surface,
     );
